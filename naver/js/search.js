a117e983bee6448527ce2dd95ec66849591caec8
SungMoonPark <taim831@naver.com>
2024-09-11 00:09:48 +0900

    first

---
+++ naver/js/search.js
@@ -0,0 +1,27 @@
+const logoBtn = document.querySelector("#logoBtn");
+const searchForm = document.querySelector("#search-form");
+const searchBar = document.querySelector("#search-bar")
+const search = document.querySelector("#search")
+const menuBtn = document.querySelector("#menuBtn")
+const LinkModal = document.querySelector("#link-modal")
+
+searchForm.addEventListener("submit", function(){
+    var query = searchBar,value;
+    var url = `https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=0&ie=utf8&query=${query}`;
+
+    window.location.href = url;
+})
+
+searchBar.addEventListener("change", function(){
+
+})
+
+menuBtn.addEventListener("click", function() {
+    LinkModal.style.display  = 'block'
+    LinkModal.showModal();
+})
+
+document.addEventListener('mouseup', function(e){
+    LinkModal.style.display  = 'none'
+    LinkModal.close();
+})
